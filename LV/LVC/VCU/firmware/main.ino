2aeb337a1b704be6669af12f1f6232412bc9fbf4
DylanMoore8 <dylancsmoore@gmail.com>
2023-10-04 20:47:57 -0300

    moved files

--- LV/LVC/VCU/firmware/main.ino
+++ LV/LVC/VCU/firmware/main.ino
@@ -1,34 +1,10 @@
-// function to check inputs for failure scenario
-bool failureCheck(){
-  return false;
-}
-
-// function to check states, should be running at all times
-void stateCheck() {
-    // set all states as false
-    // check accelerator for action
-    if (failureCheck() == true) {}
-    } else if (acceleratorVolts > 0) { // check for accelleration
-      drivingState = True;
-    } else if ((acceleratorVolts == 0) && (drivingState == True)) { // check for release of acceleration while in driving state
-      regenState = True;
-    } else if () { // 
-      
-    }
-}
-// function containing ready to drive code
-void readyToDrive() {
-  return;
-}
-// function containing regeneration code
-void regeneration() {
-  return;
-}
+#include "ready_to_drive.h"
 
 // setup, runs before main loop
 void setup () {
   Serial.begin(9600);
 }
+
 
 // main loop continuously runs
 void loop () {
@@ -37,28 +13,39 @@ void loop () {
   bool readyToDriveState = false;
   bool failureState = false;
 
-  // input simulating accellerator
-  int acceleratorSerial = analogRead(A0); // gets analog reading from pin A0 connected to potentiometer
-  float acceleratorVolts = acceleratorSerial * (5.0/1023.0); // simple unit conversion to volts from bits (1023 bit scale to 5V scale)
-  Serial.println(readVolt); // prints value to serial output terminal
+  // define inputs and outputs
+  int APPS_1_in = A1;
+  int APPS_2_in = A2;
+  int BPSF_in = A3;
+  int BPSB_in = A4;
+
+  // gets analog reading from voltage inputs
+  int APPS_1_Serial_in = analogRead(APPS_1_in);
+  int APPS_2_Serial_in = analogRead(APPS_2_in);
+  int BPSF_Serial_in = analogRead(BPSF_in);
+  int BPSB_Serial_in = analogRead(BPSB_in);
+
+  // convert serial input to volts scale
+  float APPS_1_VOLTS_in = APPS_1_Serial_in * (5.0/1023.0);
+  float APPS_2_VOLTS_in = APPS_2_Serial_in * (5.0/1023.0);
+  float BPSF_VOLTS_in = BPSF_Serial_in * (5.0/1023.0);
+  float BPSB_VOLTS_in = BPSB_Serial_in * (5.0/1023.0);
+
+  APPS_2_VOLTS_in = 0.0;
+  BPSF_VOLTS_in = 0.0;
+  BPSB_VOLTS_in = 0.0;
+
+  if ((0.5 <= APPS_1_VOLTS_in) && (4.5 >= APPS_1_VOLTS_in)) {
+    Serial.println("APPS_1_in");
+  }
+  if ((0.5 <= APPS_2_VOLTS_in) && (4.5 >= APPS_2_VOLTS_in)) {
+    Serial.println("APPS_2_in");
+  }
+  if ((0.5 <= BPSF_VOLTS_in) && (4.5 >= BPSF_VOLTS_in)) {
+    Serial.println("BPSF_in");
+  }
+  if ((0.5 <= BPSB_VOLTS_in) && (4.5 >= BPSB_VOLTS_in)) {
+    Serial.println("BPSB_in");
+  }
   
-
-  // ready to drive state (idle: awaiting input)
-  while ((readyToDriveState == true) && (failureState == false)) {
-    // check for state switch
-    stateCheck();
-  }
-  // driving state (currently in motion not)
-  while ((drivingState = true) && (failureState == false)) {
-    // check for state switch
-    stateCheck();
-
-    // regenerative breaking state
-    while ((regenState = true) && (failureState == false)) {
-      // check for state switch 
-      stateCheck();
-    }
-  }
-
-
 }
