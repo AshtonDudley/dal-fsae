56d410c856cdf6578632ae7ff14649eaa54ac49b
Aryah Kannan <68197305+shamone03@users.noreply.github.com>
2023-10-04 21:41:05 -0300

    basic analog read

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,12 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+	// put your setup code here, to run once:
+	Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+	// put your main code here, to run repeatedly:
+	Serial.println(analogRead(A1));
+	delay(1);
 }
