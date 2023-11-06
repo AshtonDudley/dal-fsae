3ed0086d99ceb91323dd69d53c3a9780a84c82ba
DylanMoore8 <dylancsmoore@gmail.com>
2023-11-05 20:38:50 -0400

    Did most of what is on tech sheet

--- LV/LVC/VCU/firmware/main/main.ino
+++ LV/LVC/VCU/firmware/main/main.ino
@@ -1,6 +1,10 @@
 //#include "ready_to_drive.h"
 #define MAXTHROTTLEVOLT 4.5
 #define MINTHROTTLEVOLT 0.5
+#define MAXBREAKVOLT 4.5
+#define MINBREAKVOLT 0.5
+#define MINREGENVOLT 0
+#define MAXREGENVOLT 0.49
 
 // converts analog inuput to voltage
 float serial_to_volt(int serial, float MAX_VOLT, int ANALOG_IN) {
@@ -22,7 +26,7 @@ bool in_range(float VALUE, float MAX_VALUE, float MIN_VALUE) {
 }
 
 // ready to drive sound function
-void readyToDriveSound(int pin_out) {
+void readyToDriveSound(const int pin_out) {
   // configure pin as output
   pinMode(pin_out, OUTPUT);
   
@@ -33,59 +37,97 @@ void readyToDriveSound(int pin_out) {
   delay(1000);
 }
 
+// define inputs
+const int APPS_1_in = A1; // accellerator pedal 1
+const int APPS_2_in = A2; // accellerator pedal 2
+const int BPSF_in = A3; // break pressure signal front 
+const int BPSB_in = A4; // break pressure signal back
+const int TQS_in = 100; // tourque system in (for faults)
+const int FWRD_in = 10; // forward drive trigger from dash
+const int RVRS_in = 11; // reverse drive trigger from dash
+const int RTDButton_in = 9; // ready to drive button from dash
+
+// define outputs
+const int FWRD_out = 5; // 
+const int RVRS_out = 6; // reverse motor signal out
+const int RTDMotorControl_out = 4; // motor controller ready to drive, initializes motor controller
+const int ACCEL_out = 7; // outputs for tourque output
+const int REGEN_out = 8; 
+
+
 // setup, runs before main loop
 void setup () {
   Serial.begin(9600);
+
+  // set pinModes for digital inputs
+  pinMode(TQS_in, INPUT);
+  pinMode(FWRD_in, INPUT);
+  pinMode(RVRS_in, INPUT);
+  pinMode(RTDButton_in, INPUT);
+
+  // set pinModes for digital outputs
+  pinMode(FWRD_in, INPUT);
+  pinMode(RVRS_in, INPUT);
+  pinMode(FWRD_out, OUTPUT);
+  pinMode(RVRS_out, OUTPUT);
+  pinMode(RTDMotorControl_out, OUTPUT);
+  pinMode(ACCEL_out, OUTPUT);
+  pinMode(REGEN_out, OUTPUT);
 }
 
-// main loop continuously runs
+
 void loop () {
-  // define inputs
-  int APPS_1_in = A1; // accellerator pedal 1
-  int APPS_2_in = A2; // accellerator pedal 2
-  int BPSF_in = A3; // break pressure signal front 
-  int BPSB_in = A4; // break pressure signal back
-  int TQS_in = NA; // tourque system in (for faults)
-  int FWRD_in = D10; // forward drive trigger from dash
-  int RVRS_in = D11; // reverse drive trigger from dash
-  
-  // define outputs
-  int FWRD_out = D5; // forward motor signal out
-  int RVRS_out = D6; // reverse motor signal out
-
-
   // gets analog reading from inputs
   int APPS_1_Serial_in = analogRead(APPS_1_in);
   int APPS_2_Serial_in = analogRead(APPS_2_in);
   int BPSF_Serial_in = analogRead(BPSF_in);
   int BPSB_Serial_in = analogRead(BPSB_in);
-  int FWRD_Serial_out = analogRead(FWRD_out);
-  int RVRS_Serial_out = analogRead(RVRS_out);
   
   // gets digital reading from inputs
   int TQS_Digital_in = digitalRead(TQS_in); 
   int FWRD_Digital_in = digitalRead(FWRD_in);
   int RVRS_Digital_in = digitalRead(RVRS_in);
-
-  // set variables withouth input (for testing)
-  BPSF_Serial_in = 0;
-  BPSB_Serial_in = 0;
+  int RTDButton_Digital_in = digitalRead(RTDButton_in);
 
   // convert serial input to volts scale
   float APPS_1_VOLTS_in = serial_to_volt(APPS_1_Serial_in, 5.0, 1023.0);
   float APPS_2_VOLTS_in = serial_to_volt(APPS_2_Serial_in, 5.0, 1023.0);
   float BPSF_VOLTS_in = serial_to_volt(BPSF_Serial_in, 5.0, 1023.0);
   float BPSB_VOLTS_in = serial_to_volt(BPSB_Serial_in, 5.0, 1023.0);
-  float FWRD_VOLTS_in = serial_to_volt(FWRD_Serial_out, 5.0, 1023.0);
-  float RVRS_VOLTS_in = serial_to_volt(RVRS_Serial_out, 5.0, 1023.0);
+
+
+
+
+  // start up mode
+
+  // check break is applied within opperating range
+  if (in_range(BPSF_VOLTS_in, MAXBREAKVOLT, MINBREAKVOLT) && in_range(BPSB_VOLTS_in, MAXBREAKVOLT, MINBREAKVOLT)) {
+    // check ready to drive button is pushed
+    if (RTDButton_Digital_in == HIGH) {
+      // TRIGGER READY TO DRIVE MODE
+      digitalWrite(RTDMotorControl_out, HIGH); // initialize motor controller
+    }
+  }
+  
+  
+  
+  
+  
+  
+  // ready to drive mode
 
   // Tourque system fault check (not aloud to drive when there is a fault)
-  if (TQS_Digital_in == LOW) {
-    
+  if (TQS_Digital_in == HIGH) {
+
+    // if (either?) break is applied tourque production must stop: APPS/Break Pedal Probability check
+    bool breaksEngaged = false;
+    if (in_range(BPSF_VOLTS_in, MAXBREAKVOLT, MINBREAKVOLT) || in_range(BPSB_VOLTS_in, MAXBREAKVOLT, MINBREAKVOLT)) {
+      breaksEngaged = true; // this is applied as a constraint while trying to supply power to the motors
+    }
+
     // check that the throttle signal maintain 5% offset from eachother: APPS implausibility check
-    float throttle_tolterance = MAXTHROTTLEVOLT*0.05;
-    bool thottlesMatch = false;
-    //
+    float throttle_tolterance = MAXTHROTTLEVOLT*0.05; // value which is 5% of max powering voltage
+    bool throttlesMatch = false;
     if ((APPS_1_VOLTS_in <= (APPS_2_VOLTS_in + throttle_tolterance)) && (APPS_1_VOLTS_in >= (APPS_2_VOLTS_in - throttle_tolterance))) {
       throttlesMatch = true;
     } else {
@@ -93,21 +135,18 @@ void loop () {
     }
 
     // check current throttle signals are within operating range
-    if (in_range(APPS_2_VOLTS_in, MAXTHROTTLEVOLT, MINTHROTTLEVOLT) && in_range(APPS_1_VOLTS_in, MAXTHROTTLEVOLT, MINTHROTTLEVOLT)) { // throttle signals within range to drive motors
-      // check if forward or reverse is active
-      if ((FWRD_Digital_in == HIGH) && thottlesMatch) {
-        FWRD_Serial_out = convert_range((APPS_1_VOLTS_in + APPS_2_VOLTS_in)/2, MAXTHROTTLEVOLT, MINTHROTTLEVOLT, 0, 1024); // thottle out 1-5 volts to the forward signal
-      } else if ((RVRS_Digital_in == HIGH) && thottlesMatch) {
-        FVRS_Serial_out = convert_range((APPS_1_VOLTS_in + APPS_2_VOLTS_in)/2, MAXTHROTTLEVOLT, MINTHROTTLEVOLT, 0, 1024); // thottle out 1-5 volts to the reverse signal
+    if (in_range(APPS_2_VOLTS_in, MAXTHROTTLEVOLT, MINTHROTTLEVOLT) && in_range(APPS_1_VOLTS_in, MAXTHROTTLEVOLT, MINTHROTTLEVOLT)) { // throttle signals within range to drive motors (might be able to change to checking just one if they are inside throttle tollerance)
+      // check if forward or reverse is active, that throttles match, and if breaks are engauged
+      if ((FWRD_Digital_in == HIGH)) {
+        digitalWrite(FWRD_out, HIGH); // tell motor controller to drive forward
+      } else if ((RVRS_Digital_in == HIGH)) {
+        digitalWrite(RVRS_out, HIGH); // tell motor controller to drive in reverse
       }
+      if (throttlesMatch && breaksEngaged == false) {
+        digitalWrite(ACCEL_out, HIGH); // signal motors to supply tourque
+      }
+    } else if (in_range(APPS_2_VOLTS_in, MAXREGENVOLT, MINREGENVOLT) && in_range(APPS_1_VOLTS_in, MAXREGENVOLT, MINREGENVOLT)) {
+      digitalWrite(REGEN_out, HIGH); // signal motors to supply regen tourque output? not sure about this one
     }
-  }
-
-  // check break signal within operating range 
-  if (in_range(BPSF_VOLTS_in, 4.5, 0.5)) {
-    Serial.println("BPSF_in");
-  }
-  if (in_range(BPSB_VOLTS_in, 4.5, 0.5)) {
-    Serial.println("BPSB_in");
   }
 }
