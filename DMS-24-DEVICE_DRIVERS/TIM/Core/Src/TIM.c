57ab8e49a8c85a6922dafc2c0cc044ba6e322b3a
Ashton Dudley <ADudley@dal.ca>
2023-12-30 22:10:14 -0400

    Added python program to covert excel lists to a c string

--- DMS-24-DEVICE_DRIVERS/TIM/Core/Src/TIM.c
+++ DMS-24-DEVICE_DRIVERS/TIM/Core/Src/TIM.c
@@ -67,7 +67,7 @@ uint16_t TIM_ConvertValue(uint16_t inputValue)
  */
 uint16_t TIM_ConvertValueLinearApprox(uint16_t inputValue)
 {
-	float xarray[] = {0.0f, 26.0f, 51.0f, 77.0f, 102.0f, 128.0f, 153.0f, 179.0f, 204.0f, 230.0f, 256.0f	};
+	float xarray[] = {0.0f, 26.0f, 51.0f, 77.0f, 102.0f, 128.0f, 153.0f, 179.0f, 204.0f, 230.0f, 256.0f};
 	//uint16_t xarray[] = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
 	float yarray[] = {0.0f, 2.5f, 7.5f, 12.5f, 20.0f, 30.0f, 40.0f, 50.0f, 60.0f, 75.0f, 100.0f};
 
